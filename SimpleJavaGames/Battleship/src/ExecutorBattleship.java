a7c7dab142e531b164f53dedcb75f45b6921636d
Alex Remizov <rem.remizov@yandex.ru>
2021-03-04 17:17:44 +0300

    Add method setUpGame in ExecutorBattleship

--- SimpleJavaGames/Battleship/src/ExecutorBattleship.java
+++ SimpleJavaGames/Battleship/src/ExecutorBattleship.java
@@ -1,4 +1,20 @@
+import java.util.ArrayList;
+
 public class ExecutorBattleship {
+    private BattleShipHelper helper = new BattleShipHelper();
+    private ArrayList<Ship> shipsList = new ArrayList<Ship>();
+    private int numberOfGuesses = 0;
+
+    private void setUpGame(){
+        Ship one = new Ship();
+        one.setName("ЛИНКОР");
+        Ship two = new Ship();
+        two.setName("КРЕЙСЕР");
+        Ship three = new Ship();
+        three.setName("БАРКАС");
+
+
+    }
 
 
 }
