f10a297eef9f90afe40e1a7d08aa5119ab6466be
Alex Remizov <rem.remizov@yandex.ru>
2021-03-05 15:43:05 +0300

    Add method main

--- SimpleJavaGames/Battleship/src/ExecutorBattleship.java
+++ SimpleJavaGames/Battleship/src/ExecutorBattleship.java
@@ -6,14 +6,59 @@ public class ExecutorBattleship {
     private int numberOfGuesses = 0;
 
     private void setUpGame(){
-        Ship one = new Ship();
-        one.setName("ЛИНКОР");
-        Ship two = new Ship();
-        two.setName("КРЕЙСЕР");
-        Ship three = new Ship();
-        three.setName("БАРКАС");
+        Ship firstShip = new Ship();
+        firstShip.setName("ЛИНКОР");
+        Ship secondShip = new Ship();
+        secondShip.setName("КРЕЙСЕР");
+        Ship thirdShip = new Ship();
+        thirdShip.setName("БАРКАС");
+        shipsList.add(firstShip);
+        shipsList.add(secondShip);
+        shipsList.add(thirdShip);
+        System.out.println("ВАША ЦЕЛЬ ПОТОПИТЬ : ЛИНКОР,КРЕЙСЕР,БАРКАС ИСПОЛЬЗУЯ МИНИМУМ ХОДОВ!");
+        for (Ship shipsToSet:
+             shipsList){
+            ArrayList<String> newLocation = helper.movieShips(3);
+            shipsToSet.setLocationCells(newLocation);
+        }
+    }
 
+    private void startPlaying(){
+        while (!shipsList.isEmpty()){
+            String userGuess = helper.getUserInput("СДЕЛАЙТЕ ХОД!");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
 
+    private void checkUserGuess( String userGuess){
+        numberOfGuesses++;
+        String result = "МИМО!";
+        for( Ship shipToTest : shipsList){
+            result = shipToTest.checkYourselfAnswer(userGuess);
+            if (result.equals("ПОПАЛ!")){
+                break;
+            }if(result.equals("ПОТОПИЛ!")){
+                shipsList.remove(shipToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame(){
+        System.out.println("ВСЕ КОРАБЛИ УШЛИ КО ДНУ!");
+        if (numberOfGuesses <= 18) {
+            System.out.println("ЭТО ЗАНЯЛО У ВАС ВСЕГО " + numberOfGuesses + "ПОПЫТОК.");
+        }else {
+            System.out.println("ЭТО ЗАНЯЛО УВАС ДОВОЛЬНО МНОГО " + numberOfGuesses + "ПОПЫТОК.");
+        }
+    }
+
+    public static void main(String[] args) {
+        ExecutorBattleship battleship = new ExecutorBattleship();
+        battleship.setUpGame();
+        battleship.startPlaying();
     }
 
 
