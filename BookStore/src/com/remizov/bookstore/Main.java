e0e0922d8e7cac94f525fabc541d75f4fdc180aa
Alex Remizov <rem.remizov@yandex.ru>
2021-04-27 11:09:17 +0300

    Start project BookStore

---
+++ BookStore/src/com/remizov/bookstore/Main.java
@@ -0,0 +1,7 @@
+package com.remizov.bookstore;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ BookStore/src/com/remizov/bookstore/entity/Person.java
@@ -0,0 +1,13 @@
+package com.remizov.bookstore.entity;
+
+public class Person {
+    private long id;
+    private String name;
+    private int age;
+
+    public Person(long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
