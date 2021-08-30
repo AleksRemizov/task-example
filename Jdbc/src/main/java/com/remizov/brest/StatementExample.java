64cc2d5dad1e7bd372169fa615ae4b74a71c39e3
Alex Remizov <rem.remizov@yandex.ru>
2021-08-30 16:12:35 +0300

    Add ResultSet update/insert row

--- Jdbc/src/main/java/com/remizov/brest/StatementExample.java
+++ Jdbc/src/main/java/com/remizov/brest/StatementExample.java
@@ -19,18 +19,32 @@ public class StatementExample {
         properties.put("autoReconnect","true");
         properties.put("characterEncoding","UTF-8");
         properties.put("useUnicode","true");
-        properties.put("cacheDefaultTimeZone","true");
+        /*properties.put("cacheDefaultTimeZone","true");
         properties.put("continueBatchOnError","true");
         properties.put("dontTrackOpenResources","false");
-        properties.put("queryTimeoutKillsConnection","false");
+        properties.put("queryTimeoutKillsConnection","false");*/
 
         try (Connection connection = DriverManager.getConnection(url,properties);
-             Statement statement =  connection.createStatement()){
+             Statement statement =  connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
+                                                                ResultSet.CONCUR_UPDATABLE)){
             String sql = "SELECT IDphonebook, lastname, phone FROM phonebook";
             ResultSet resultSet = statement.executeQuery(sql);
             List<Subscriber> subscribers = new ArrayList<>();
+            //insert row
+            resultSet.moveToInsertRow();
+            resultSet.updateInt(1,22);
+            resultSet.updateString(2,"Petrov");
+            resultSet.updateInt(3,101);
+            resultSet.insertRow();
+            resultSet.moveToCurrentRow();
+
             while (resultSet.next()){
+                // update row
                 int id = resultSet.getInt(1);
+                if(id == 1){
+                    resultSet.updateInt("phone",777777);
+                    resultSet.updateRow();
+                }
                 String name = resultSet.getString(2);
                 int phone = resultSet.getInt("phone");
                 subscribers.add(new Subscriber(id,name,phone));
